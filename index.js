0911c51b53224844d993768f360c3982d890a956
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 04:31:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ function Scooter(year, color, model){
   this.color=color;
   this.model=model;
 }
-function Driver(name, age, model){
+function Driver(name, age, experience){
+  this.name=name;
+  this.age=age;
+  this.experience=experience;
+}
+function Scooter(year, color, model){
   this.year=year;
   this.color=color;
   this.model=model;
