158740bebe33c2be6b2290f754d53bcf94ea5c72
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-08 04:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function Driver(name, age, experience){
   this.age=age;
   this.experience=experience;
 }
-function Scooter(year, color, model){
-  this.year=year;
-  this.color=color;
-  this.model=model;
+function PickupLocation(address, city){
+  this.address=address;
+  this.city=city;
 }
